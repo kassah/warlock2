74288d793d2f8af3ad93df60982e0d9936b971fd
Marshall Culpepper <marshall@arcaner.com>
2007-04-08 07:46:45 +0000

    an abstract property listener for convenience
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@124 bb130017-240f-0410-9613-9c2c827820fb

---
+++ warlock2-rcp/com.arcaner.warlock.core/src/main/com/arcaner/warlock/client/PropertyListener.java
@@ -0,0 +1,20 @@
+package com.arcaner.warlock.client;
+
+public class PropertyListener<T> implements IPropertyListener<T> {
+
+	public void propertyActivated(IProperty<T> property) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void propertyChanged(IProperty<T> property, T oldValue) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void propertyCleared(IProperty<T> property, T oldValue) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
