214a233c66dea171350b96a75ae07b663a728597
Marshall Culpepper <marshall@arcaner.com>
2007-04-12 17:56:02 +0000

    the GameView is now just a specialized stream view with an entry and compass..
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@169 bb130017-240f-0410-9613-9c2c827820fb

--- warlock2-rcp/com.arcaner.warlock.rcp/src/com/arcaner/warlock/rcp/views/GameView.java
+++ warlock2-rcp/com.arcaner.warlock.rcp/src/com/arcaner/warlock/rcp/views/GameView.java
@@ -9,7 +9,6 @@ import java.util.Random;
 
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.StyleRange;
 import org.eclipse.swt.events.FocusEvent;
 import org.eclipse.swt.events.FocusListener;
 import org.eclipse.swt.events.KeyEvent;
@@ -17,20 +16,14 @@ import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.part.ViewPart;
 
 import com.arcaner.warlock.client.IWarlockClient;
-import com.arcaner.warlock.client.IWarlockClientViewer;
-import com.arcaner.warlock.client.stormfront.AbstractStormFrontClientViewer;
 import com.arcaner.warlock.client.stormfront.IStormFrontClient;
-import com.arcaner.warlock.client.stormfront.IStormFrontStyle;
+import com.arcaner.warlock.client.stormfront.IStormFrontClientViewer;
 import com.arcaner.warlock.client.stormfront.WarlockColor;
 import com.arcaner.warlock.configuration.ServerSettings;
 import com.arcaner.warlock.rcp.ui.WarlockCompass;
@@ -39,14 +32,11 @@ import com.arcaner.warlock.rcp.ui.client.SWTStormFrontClientViewer;
 import com.arcaner.warlock.rcp.ui.macros.IMacro;
 import com.arcaner.warlock.rcp.ui.macros.MacroFactory;
 import com.arcaner.warlock.rcp.ui.style.CompassThemes;
-import com.arcaner.warlock.rcp.ui.style.StyleMappings;
-import com.arcaner.warlock.stormfront.IStream;
-import com.arcaner.warlock.stormfront.internal.Stream;
 
 /**
  * @author marshall
  */
-public class GameView extends ViewPart implements KeyListener {
+public class GameView extends StreamView implements KeyListener, IStormFrontClientViewer {
 
 	public static final String VIEW_ID = "com.arcaner.warlock.rcp.views.gameView";
 	
@@ -55,23 +45,24 @@ public class GameView extends ViewPart implements KeyListener {
 	private static ArrayList<GameView> openViews = new ArrayList<GameView>();
 	private static GameView viewInFocus;
 	
-	protected WarlockText text;
 	protected Text entry;
 	protected WarlockCompass compass;
 	
-	protected IStormFrontClient client;
-	protected ViewEvents viewer;
-	private Font normalFont = JFaceResources.getDefaultFont(), monospaceFont = JFaceResources.getDefaultFont();
+	protected SWTStormFrontClientViewer wrapper;
+//	protected ViewEvents viewer;
+	private Font normalFont = JFaceResources.getDefaultFont();
 	
-	public GameView() {
+	public GameView () {
 		if (firstInstance == null)
 			firstInstance = this;
 		
-		viewer = new ViewEvents(client);
+		wrapper = new SWTStormFrontClientViewer(this);
+//		viewer = new ViewEvents(client);
 		openViews.add(this);
+		setStreamName(IWarlockClient.DEFAULT_STREAM_NAME);
 	}
 	
-	public static Collection<GameView> getOpenViews ()
+	public static Collection<GameView> getOpenGameViews ()
 	{
 		return openViews;
 	}
@@ -104,75 +95,62 @@ public class GameView extends ViewPart implements KeyListener {
 		return null;
 	}
 	
-	private class ViewEvents extends AbstractStormFrontClientViewer {
-		
-		private SWTStormFrontClientViewer viewerWrapper;
-		
-		public ViewEvents (IStormFrontClient client)
-		{
-			super(client);
-			
-			viewerWrapper = new SWTStormFrontClientViewer(this);
-		}
-		
-		public void append (IStream stream, String toAppend, IStormFrontStyle style) {
-			GameView.this.append(stream, toAppend, style);
-		}
-		
-		public void echo (IStream stream, String text, IStormFrontStyle style) {
-			GameView.this.echo(stream, text, style);
-		}
-		
-		public void setViewerTitle(String title) {
-			GameView.this.setViewerTitle(title);
-		}
-		
-		public String getCurrentCommand ()
-		{
-			return GameView.this.getCurrentCommand();
-		}
-		
-		public void setClient (IStormFrontClient client)
-		{
-			this.client = client;
-			
-			client.addViewer(viewerWrapper);
-		}
-
-		public SWTStormFrontClientViewer getViewerWrapper() {
-			return viewerWrapper;
-		}
-		
-		public void loadServerSettings (ServerSettings settings)
-		{
-			GameView.this.loadServerSettings(settings);
-		}
-		
-		public void setCurrentCommand(String command) {
-			entry.setText(command);
-		}
-	}
+//	private class ViewEvents implements IStormFrontClientViewer {
+//		
+//		private SWTStormFrontClientViewer viewerWrapper;
+//		
+//		public ViewEvents (IStormFrontClient client)
+//		{
+//			super(client);
+//			
+//			viewerWrapper = new SWTStormFrontClientViewer(this);
+//		}
+//		
+//		public void append (IStream stream, String toAppend, IWarlockStyle style) {
+//			GameView.this.append(stream, toAppend, style);
+//		}
+//		
+//		public void echo (IStream stream, String text, IWarlockStyle style) {
+//			GameView.this.echo(stream, text, style);
+//		}
+//		
+//		public void setViewerTitle(String title) {
+//			GameView.this.setViewerTitle(title);
+//		}
+//		
+//		public String getCurrentCommand ()
+//		{
+//			return GameView.this.getCurrentCommand();
+//		}
+//		
+//		public void setClient (IStormFrontClient client)
+//		{
+//			this.client = client;
+//			
+//			client.addViewer(viewerWrapper);
+//		}
+//
+//		public SWTStormFrontClientViewer getViewerWrapper() {
+//			return viewerWrapper;
+//		}
+//		
+//		public void loadServerSettings (ServerSettings settings)
+//		{
+//			GameView.this.loadServerSettings(settings);
+//		}
+//		
+//		public void setCurrentCommand(String command) {
+//			entry.setText(command);
+//		}
+//	}
 	
 	private static String generateUniqueId () {
 		return new Random().nextInt() + "";
 	}
 	
 	public void createPartControl(Composite parent) {
+		super.createPartControl(parent);
 		
-		Composite top = new Composite (parent, SWT.NONE);
-		GridLayout layout = new GridLayout(1, false);
-		layout.marginHeight = 0;
-		layout.marginWidth = 0;
-		layout.horizontalSpacing = 0;
-		layout.verticalSpacing = 0;
-		top.setLayout(layout);
-		
-		text = new WarlockText(top, SWT.V_SCROLL);
-		text.setLayoutData(new GridData(GridData.FILL, GridData.FILL, true, true));
-		text.setEditable(false);
-		text.setWordWrap(true);
-		text.setBackground(new Color(text.getDisplay(), 25, 25, 50));
-		text.setForeground(new Color(text.getDisplay(), 240, 240, 255));
 		text.addFocusListener(new FocusListener () { 
 			public void focusGained(FocusEvent e) {
 				entry.setFocus();
@@ -189,12 +167,8 @@ public class GameView extends ViewPart implements KeyListener {
 		
 		text.addLink("http://warlock.sf.net", "The Warlock Team");
 		
-//		Composite entryComposite = new Composite(top, SWT.NONE);
-//		entryComposite.setLayout(new GridLayout(1, false));
-//		
-		entry = new Text(top, SWT.BORDER);
+		entry = new Text(mainComposite, SWT.BORDER);
 		entry.setLayoutData(new GridData(GridData.FILL, GridData.VERTICAL_ALIGN_END, true, false, 1, 1));
-		//entry.setLayoutData(new RowData(250, 15));
 		entry.addKeyListener(this);
 		
 		compass = new WarlockCompass(text, CompassThemes.getCompassTheme("small"));
@@ -205,41 +179,6 @@ public class GameView extends ViewPart implements KeyListener {
 		entry.setFocus();
 		
 		viewInFocus = this;
-	}
-	
-	public void append (IStream stream, String toAppend, IStormFrontStyle style)
-	{
-		StyleRange range = StyleMappings.getStyle(client.getServerSettings(), style, text.getCharCount(), toAppend.length());
-		
-		if (stream.equals(Stream.DEFAULT_STREAM))
-		{
-			text.append(toAppend);
-			
-			if (range != null)
-				text.setStyleRange(range);
-			
-			int length = text.getContent().getCharCount();		
-			if (text.getCaretOffset() < length) {
-				text.setCaretOffset(length);
-				text.showSelection();
-			}
-		}
-	}
-	
-	public void echo (IStream stream, String text, IStormFrontStyle style)
-	{
-		// A hacked style range, for now. This was mainly so we could get an idea on how to do it
-		// once we implement highlight prefs, etc.
-		
-		StyleRange range = new StyleRange();
-		range.background = new Color(getSite().getShell().getDisplay(), 0x30, 0x30, 0x30);
-		range.foreground = new Color(getSite().getShell().getDisplay(), 255, 255, 255);
-		range.fontStyle = SWT.BOLD;
-		range.start = GameView.this.text.getCharCount();
-		range.length = text.length();
-		
-		this.text.append(text);
-		this.text.setStyleRange(range);
 	}
 	
 	private Color createColor (WarlockColor color)
@@ -254,11 +193,8 @@ public class GameView extends ViewPart implements KeyListener {
 		
 		String fontFace = settings.getStringSetting(ServerSettings.StringType.MainWindow_FontFace);
 		int fontSize = settings.getIntSetting(ServerSettings.IntType.MainWindow_FontSize);
-		String monoFontFace = settings.getStringSetting(ServerSettings.StringType.MainWindow_MonoFontFace);
-		int monoFontSize = settings.getIntSetting(ServerSettings.IntType.MainWindow_MonoFontSize);
 		
-		normalFont = fontFace == null ? JFaceResources.getDefaultFont() : new Font(getSite().getShell().getDisplay(), fontFace, fontSize-2, SWT.NONE);
-		monospaceFont = monoFontFace == null ? JFaceResources.getDialogFont() : new Font(getSite().getShell().getDisplay(), monoFontFace, monoFontSize-2, SWT.NONE);
+		normalFont = fontFace == null ? JFaceResources.getDefaultFont() : new Font(getSite().getShell().getDisplay(), fontFace, fontSize, SWT.NONE);
 		text.setFont(normalFont);
 		
 		WarlockColor entryBG = settings.getColorSetting(ServerSettings.ColorType.CommandLine_Background);
@@ -276,9 +212,15 @@ public class GameView extends ViewPart implements KeyListener {
 		return GameView.this.entry.getText();
 	}
 	
+	public void setCurrentCommand(String command) {
+		GameView.this.entry.setText(command);
+	}
+	
 	public void setStormFrontClient(IStormFrontClient client) {
 		this.client = client;
-		viewer.setClient(client);
+		
+		setStream(client.getDefaultStream());
+		
 		text.setText("");
 		
 		compass.setCompass(client.getCompass());
@@ -287,17 +229,22 @@ public class GameView extends ViewPart implements KeyListener {
 			BarsView.getDefault().init(client);
 		
 		for (StreamView streamView : StreamView.getOpenViews())
-			streamView.setClient(client);
+		{
+			if (streamView != this) {
+				streamView.setClient(client);
+			}
+		}
 	}
 	
-	public void keyPressed(KeyEvent e) {
-
-	}
-	
-	public IWarlockClient getClient() {
+	public IWarlockClient getWarlockClient() {
 		return client;
 	}
 	
+	public IStormFrontClient getStormFrontClient() {
+		return client;
+	}
+
+	public void keyPressed(KeyEvent e) {}
 	public void keyReleased(KeyEvent e) {
 		Collection<IMacro> macros = MacroFactory.instance().getMacros();
 		
@@ -305,7 +252,7 @@ public class GameView extends ViewPart implements KeyListener {
 		{
 			if (macro.getKeyCode() == e.keyCode)
 			{
-				String result = macro.executeCommand(viewer.getViewerWrapper());
+				String result = macro.executeCommand(wrapper);
 				if (result != null)
 				{
 					entry.setText(result);
@@ -315,10 +262,5 @@ public class GameView extends ViewPart implements KeyListener {
 			}
 		}
 		
-	}
-	
-	public IWarlockClientViewer getViewer ()
-	{
-		return viewer;
 	}
 }
