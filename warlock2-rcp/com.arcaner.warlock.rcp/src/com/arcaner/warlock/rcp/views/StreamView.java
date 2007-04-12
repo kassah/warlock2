fd8327d48f215cdad148d60ce186770e7f82691a
Marshall Culpepper <marshall@arcaner.com>
2007-04-12 17:55:22 +0000

    we now return a pre-existing view if one for the passed in stream name already exists. majorly refactored for the new stream based viewer api
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@168 bb130017-240f-0410-9613-9c2c827820fb

--- warlock2-rcp/com.arcaner.warlock.rcp/src/com/arcaner/warlock/rcp/views/StreamView.java
+++ warlock2-rcp/com.arcaner.warlock.rcp/src/com/arcaner/warlock/rcp/views/StreamView.java
@@ -2,11 +2,12 @@ package com.arcaner.warlock.rcp.views;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Random;
+import java.util.Hashtable;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.LineBackgroundEvent;
+import org.eclipse.swt.custom.LineBackgroundListener;
 import org.eclipse.swt.custom.StyleRange;
-import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.layout.GridData;
@@ -17,31 +18,55 @@ import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 
+import com.arcaner.warlock.client.IProperty;
+import com.arcaner.warlock.client.IStream;
+import com.arcaner.warlock.client.IStreamListener;
+import com.arcaner.warlock.client.IWarlockStyle;
+import com.arcaner.warlock.client.PropertyListener;
 import com.arcaner.warlock.client.stormfront.IStormFrontClient;
-import com.arcaner.warlock.client.stormfront.IStormFrontStyle;
 import com.arcaner.warlock.client.stormfront.WarlockColor;
 import com.arcaner.warlock.configuration.ServerSettings;
+import com.arcaner.warlock.rcp.ui.StyleRangeWithData;
+import com.arcaner.warlock.rcp.ui.WarlockText;
+import com.arcaner.warlock.rcp.ui.client.SWTPropertyListener;
 import com.arcaner.warlock.rcp.ui.client.SWTStreamListener;
 import com.arcaner.warlock.rcp.ui.style.StyleMappings;
-import com.arcaner.warlock.stormfront.IStream;
-import com.arcaner.warlock.stormfront.IStreamListener;
 
-public class StreamView extends ViewPart implements IStreamListener{
+public class StreamView extends ViewPart implements IStreamListener, LineBackgroundListener {
 	public static final String VIEW_ID = "com.arcaner.warlock.rcp.views.StreamView";
-	private static ArrayList<StreamView> openViews = new ArrayList<StreamView>();
+	protected static ArrayList<StreamView> openViews = new ArrayList<StreamView>();
 	
-	private IStream stream;
-	private IStormFrontClient client;
-	private StyledText text;
+	protected IStream stream;
+	protected IStormFrontClient client;
+	protected WarlockText text;
+	protected Hashtable<Integer, Color> lineBackgrounds = new Hashtable<Integer,Color>();
+	protected Composite mainComposite;
+	// This name is the 'suffix' part of the stream... so we will install listeners for each client
+	protected String streamName;
+	protected SWTStreamListener streamListenerWrapper;
+	protected SWTPropertyListener<String> propertyListenerWrapper;
 	
 	public StreamView() {
 		openViews.add(this);
 	}
 
-	public static StreamView createNext (String streamName) {
+	public static StreamView getViewForStream (String streamName) {
 		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		
+		for (StreamView view : openViews)
+		{
+			if (view.getStreamName().equals(streamName))
+			{
+				page.activate(view);
+				return view;
+			}
+		}
+		
+		// none of the already created views match, create a new one
 		try {
 			StreamView nextInstance = (StreamView) page.showView(VIEW_ID, streamName, IWorkbenchPage.VIEW_ACTIVATE);
+			nextInstance.setStreamName(streamName);
+			
 			return nextInstance;
 		} catch (PartInitException e) {
 			e.printStackTrace();
@@ -51,20 +76,27 @@ public class StreamView extends ViewPart implements IStreamListener{
 	
 	@Override
 	public void createPartControl(Composite parent) {
-		Composite top = new Composite (parent, SWT.NONE);
+		mainComposite = new Composite (parent, SWT.NONE);
 		GridLayout layout = new GridLayout(1, false);
 		layout.marginHeight = 0;
 		layout.marginWidth = 0;
 		layout.horizontalSpacing = 0;
 		layout.verticalSpacing = 0;
-		top.setLayout(layout);
+		mainComposite.setLayout(layout);
 		
-		text = new StyledText(top, SWT.V_SCROLL);
+		text = new WarlockText(mainComposite, SWT.V_SCROLL);
 		text.setLayoutData(new GridData(GridData.FILL, GridData.FILL, true, true));
 		text.setEditable(false);
 		text.setWordWrap(true);
 		text.setBackground(new Color(text.getDisplay(), 25, 25, 50));
 		text.setForeground(new Color(text.getDisplay(), 240, 240, 255));
+		text.addLineBackgroundListener(this);
+		
+		GameView currentGameView = GameView.getViewInFocus();
+		if (currentGameView != null && currentGameView.getStormFrontClient() != null)
+		{
+			setPartName(currentGameView.getStormFrontClient().getStream(streamName).getTitle().get());
+		}
 	}
 
 	@Override
@@ -80,22 +112,86 @@ public class StreamView extends ViewPart implements IStreamListener{
 	public void setStream(IStream stream) {
 		this.stream = stream;
 		
-		setPartName(stream.getTitle());
-		stream.addStreamListener(new SWTStreamListener(this));
+		streamListenerWrapper = new SWTStreamListener(this);
+		stream.addStreamListener(streamListenerWrapper);
+		propertyListenerWrapper = new SWTPropertyListener<String>(new PropertyListener<String>() {
+			@Override
+			public void propertyChanged(IProperty<String> property, String oldValue) {
+				if (property.getName().equals("streamTitle"))
+				{
+					setPartName(property.get());
+				}
+			}
+		});
+		stream.getTitle().addListener(propertyListenerWrapper);
 	}
 	
-	public void streamCleared() {
-		text.setText("");
+	
+	public void streamCleared(IStream stream) {
+		if (this.stream.equals(stream))
+			text.setText("");
 	}
 	
-	public void streamReceivedText(String text, IStormFrontStyle style) {
-		text = text + "\n";
+	private void scrollToBottom ()
+	{
+		int length = this.text.getContent().getCharCount();		
+		if (this.text.getCaretOffset() < length) {
+			this.text.setCaretOffset(length);
+			this.text.showSelection();
+		}
+	}
+	
+	public void streamReceivedText(IStream stream, String text, IWarlockStyle style) {
+		if (this.stream.equals(stream))
+		{
+			StyleRangeWithData range = null;
+			try {
+				range = StyleMappings.getStyle(client.getServerSettings(), style, this.text.getCharCount(), text.length());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			this.text.append(text);
+			if (range != null) {
+				this.text.setStyleRange(range);
+				if (range.data.containsKey(StyleMappings.FILL_ENTIRE_LINE))
+				{
+					int lineIndex = this.text.getLineAtOffset(range.start);
+					lineBackgrounds.put(lineIndex, range.background);
+				}
+			}
+			
+			scrollToBottom();
+		}
+	}
+	
+	public void lineGetBackground(LineBackgroundEvent event) {
+		int lineIndex = this.text.getLineAtOffset(event.lineOffset);
 		
-		StyleRange range = StyleMappings.getStyle(client.getServerSettings(), style, this.text.getCharCount(), text.length());
-		
-		this.text.append(text);
-		if (range != null)
-			this.text.setStyleRange(range);
+		if (lineBackgrounds.containsKey(lineIndex))
+		{
+			event.lineBackground = lineBackgrounds.get(lineIndex);
+		}
+	}
+	
+	public void streamEchoed(IStream stream, String text) {
+		if (this.stream.equals(stream))
+		{
+			StyleRange echoStyle = StyleMappings.getEchoStyle(this.text.getCharCount(), text.length());
+			this.text.append(text + "\n");
+			this.text.setStyleRange(echoStyle);
+			
+			scrollToBottom();
+		}
+	}
+	
+	public void streamPrompted(IStream stream, String prompt) {
+		if (this.stream.equals(stream))
+		{
+			this.text.append(prompt);
+			scrollToBottom();
+		}
 	}
 
 	public static Collection<StreamView> getOpenViews ()
@@ -111,6 +207,8 @@ public class StreamView extends ViewPart implements IStreamListener{
 	public void setClient (IStormFrontClient client)
 	{
 		this.client = client;
+		
+		setStream(client.getStream(streamName));
 	}
 	
 	public void loadServerSettings (ServerSettings settings)
@@ -124,13 +222,22 @@ public class StreamView extends ViewPart implements IStreamListener{
 		text.setBackground(createColor(bg));
 		text.setForeground(createColor(fg));
 		
-		Font normalFont = new Font(getSite().getShell().getDisplay(), fontFace, fontSize-2, SWT.NONE);
+		Font normalFont = new Font(getSite().getShell().getDisplay(), fontFace, fontSize, SWT.NONE);
 		text.setFont(normalFont);
 	}
 	
 	@Override
 	public void dispose() {
-		stream.removeStreamListener(this);
+		stream.removeStreamListener(streamListenerWrapper);
+		stream.getTitle().removeListener(propertyListenerWrapper);
 		super.dispose();
+	}
+
+	public String getStreamName() {
+		return streamName;
+	}
+
+	public void setStreamName(String streamName) {
+		this.streamName = streamName;
 	}
 }
