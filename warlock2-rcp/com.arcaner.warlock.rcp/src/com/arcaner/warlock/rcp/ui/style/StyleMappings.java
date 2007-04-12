72c9e39d3203d84635767d1f8ca64b0319ef5415
Marshall Culpepper <marshall@arcaner.com>
2007-04-12 17:56:48 +0000

    added support for styles that 'fill the entire line' of the text entry
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@170 bb130017-240f-0410-9613-9c2c827820fb

--- warlock2-rcp/com.arcaner.warlock.rcp/src/com/arcaner/warlock/rcp/ui/style/StyleMappings.java
+++ warlock2-rcp/com.arcaner.warlock.rcp/src/com/arcaner/warlock/rcp/ui/style/StyleMappings.java
@@ -7,34 +7,40 @@ import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.widgets.Display;
 
-import com.arcaner.warlock.client.stormfront.IStormFrontStyle;
+import com.arcaner.warlock.client.IWarlockStyle;
 import com.arcaner.warlock.client.stormfront.WarlockColor;
 import com.arcaner.warlock.configuration.ServerSettings;
+import com.arcaner.warlock.rcp.ui.StyleRangeWithData;
 
 public class StyleMappings {
 
-	public static StyleRange getStyle (ServerSettings settings, IStormFrontStyle style, int start, int length)
+	public static final String FILL_ENTIRE_LINE = "fillEntireLine";
+	
+	public static StyleRangeWithData getStyle (ServerSettings settings, IWarlockStyle style, int start, int length)
 	{
+		if (style.getStyleTypes().contains(IWarlockStyle.StyleType.EMPTY))
+			return null;
+		
 		Display display = Display.getDefault();
-		StyleRange range = new StyleRange();
+		StyleRangeWithData range = new StyleRangeWithData();
 		range.start = start;
 		range.length = length;
 		range.fontStyle = 0;
 		
-		for (IStormFrontStyle.StyleType styleType : style.getStyleTypes())
+		for (IWarlockStyle.StyleType styleType : style.getStyleTypes())
 		{
-			if (styleType.equals(IStormFrontStyle.StyleType.BOLD))
+			if (styleType.equals(IWarlockStyle.StyleType.BOLD))
 				range.fontStyle |= SWT.BOLD;
-			else if (styleType.equals(IStormFrontStyle.StyleType.ITALIC))
+			else if (styleType.equals(IWarlockStyle.StyleType.ITALIC))
 				range.fontStyle |= SWT.ITALIC;
-			else if (styleType.equals(IStormFrontStyle.StyleType.UNDERLINE))
+			else if (styleType.equals(IWarlockStyle.StyleType.UNDERLINE))
 				range.underline = true;
-			else if (styleType.equals(IStormFrontStyle.StyleType.MONOSPACE))
+			else if (styleType.equals(IWarlockStyle.StyleType.MONOSPACE))
 			{
 				String monoFontFace = settings.getStringSetting(ServerSettings.StringType.MainWindow_MonoFontFace);
 				int monoFontSize = settings.getIntSetting(ServerSettings.IntType.MainWindow_MonoFontSize);
 				
-				range.font = monoFontFace == null ? JFaceResources.getDialogFont() : new Font(display, monoFontFace, monoFontSize-2, SWT.NONE);
+				range.font = monoFontFace == null ? JFaceResources.getDialogFont() : new Font(display, monoFontFace, monoFontSize, SWT.NONE);
 			}
 		}
 		
@@ -56,6 +62,25 @@ public class StyleMappings {
 			range.foreground = new Color(display, foreground.getRed(), foreground.getGreen(), foreground.getBlue());
 		if (background != WarlockColor.DEFAULT_COLOR)
 			range.background = new Color(display, background.getRed(), background.getGreen(), background.getBlue());
+		
+		if (settings.getPresetFillEntireLine(style.getStyleName()))
+		{
+			range.data.put(FILL_ENTIRE_LINE, "true");
+		}
+		
+		return range;
+	}
+	
+	public static StyleRange getEchoStyle (int start, int length)
+	{
+		Display display = Display.getDefault();
+		
+		StyleRange range = new StyleRange();
+		range.background = new Color(display, 0x30, 0x30, 0x30);
+		range.foreground = new Color(display, 255, 255, 255);
+		range.fontStyle = SWT.BOLD;
+		range.start = start;
+		range.length = length;
 		
 		return range;
 	}
