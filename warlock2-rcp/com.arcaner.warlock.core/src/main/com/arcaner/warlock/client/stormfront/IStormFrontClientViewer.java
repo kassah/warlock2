331a851af9b55dc960e8e79202717b1b88918f32
Marshall Culpepper <marshall@arcaner.com>
2007-05-20 05:32:59 +0000

    support for package refactors
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@290 bb130017-240f-0410-9613-9c2c827820fb

--- warlock2-rcp/com.arcaner.warlock.core/src/main/com/arcaner/warlock/client/stormfront/IStormFrontClientViewer.java
+++ warlock2-rcp/com.arcaner.warlock.core/src/main/com/arcaner/warlock/client/stormfront/IStormFrontClientViewer.java
@@ -1,7 +1,7 @@
 package com.arcaner.warlock.client.stormfront;
 
 import com.arcaner.warlock.client.IWarlockClientViewer;
-import com.arcaner.warlock.configuration.ServerSettings;
+import com.arcaner.warlock.configuration.server.ServerSettings;
 
 public interface IStormFrontClientViewer extends IWarlockClientViewer {
 
