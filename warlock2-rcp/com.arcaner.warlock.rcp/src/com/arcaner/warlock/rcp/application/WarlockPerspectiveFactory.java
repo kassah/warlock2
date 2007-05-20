f5e6a240a18956380c7191dbc2ee38a93b96d925
Marshall Culpepper <marshall@arcaner.com>
2007-05-20 05:35:59 +0000

    placeholders have been added for all of the default stream windows so far so opening them should yield a much more "sane" default.
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@293 bb130017-240f-0410-9613-9c2c827820fb

--- warlock2-rcp/com.arcaner.warlock.rcp/src/com/arcaner/warlock/rcp/application/WarlockPerspectiveFactory.java
+++ warlock2-rcp/com.arcaner.warlock.rcp/src/com/arcaner/warlock/rcp/application/WarlockPerspectiveFactory.java
@@ -8,9 +8,12 @@ package com.arcaner.warlock.rcp.application;
 
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.IPlaceholderFolderLayout;
 
+import com.arcaner.warlock.client.stormfront.IStormFrontClient;
 import com.arcaner.warlock.rcp.views.BarsView;
 import com.arcaner.warlock.rcp.views.GameView;
+import com.arcaner.warlock.rcp.views.StreamView;
 
 /**
  * @author Marshall
@@ -21,6 +24,9 @@ public class WarlockPerspectiveFactory implements IPerspectiveFactory {
 	
 	public static final String WARLOCK_PERSPECTIVE_ID = "com.arcaner.warlock.warlockPerspective";
 	public static final String BOTTOM_FOLDER_ID = "com.arcaner.warlock.bottomFolder";
+	public static final String TOP_FOLDER_ID = "com.arcaner.warlock.topFolder";
+	public static final String RIGHT_FOLDER_ID = "com.arcaner.warlock.rightFolder";
+	public static final String LEFT_FOLDER_ID = "com.arcaner.warlock.leftFolder";
 	
 	public static IPageLayout getLayout() {
 		return myLayout;
@@ -34,6 +40,18 @@ public class WarlockPerspectiveFactory implements IPerspectiveFactory {
 		
 		layout.setEditorAreaVisible(false);
 		layout.addView(GameView.VIEW_ID, IPageLayout.LEFT, 0.15f, layout.getEditorArea());
+		
+		IPlaceholderFolderLayout topFolder =
+			layout.createPlaceholderFolder(TOP_FOLDER_ID, IPageLayout.TOP, 0.15f, GameView.VIEW_ID);
+		
+		topFolder.addPlaceholder(StreamView.DEATH_VIEW_ID);
+		topFolder.addPlaceholder(StreamView.THOUGHTS_VIEW_ID);
+		
+		IPlaceholderFolderLayout rightFolder =
+			layout.createPlaceholderFolder(RIGHT_FOLDER_ID, IPageLayout.RIGHT, 0.80f, GameView.VIEW_ID);
+		
+		rightFolder.addPlaceholder(StreamView.INVENTORY_VIEW_ID);
+		
 //		IFolderLayout folder = layout.createFolder(BOTTOM_FOLDER_ID, IPageLayout.BOTTOM, 0.90f, GameView.VIEW_ID);
 		layout.addStandaloneView(BarsView.VIEW_ID, false, IPageLayout.BOTTOM, 0.95f, GameView.VIEW_ID);
 		
