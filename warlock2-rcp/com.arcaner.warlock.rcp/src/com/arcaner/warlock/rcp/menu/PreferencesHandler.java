27953bf2779a15e0d73780248ba8ea5952503f1c
Marshall Culpepper <marshall@arcaner.com>
2007-05-20 05:36:57 +0000

    our preferences are anchored on the logged in IStormfrontClient now. the preferences menu item will be disabled until you login for now.
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@294 bb130017-240f-0410-9613-9c2c827820fb

--- warlock2-rcp/com.arcaner.warlock.rcp/src/com/arcaner/warlock/rcp/menu/PreferencesHandler.java
+++ warlock2-rcp/com.arcaner.warlock.rcp/src/com/arcaner/warlock/rcp/menu/PreferencesHandler.java
@@ -2,36 +2,63 @@ package com.arcaner.warlock.rcp.menu;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.preference.IPreferenceNode;
 import org.eclipse.jface.preference.PreferenceDialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.dialogs.PreferencesUtil;
 
-import com.arcaner.warlock.rcp.plugin.Warlock2Plugin;
+import com.arcaner.warlock.client.stormfront.IStormFrontClient;
+import com.arcaner.warlock.rcp.prefs.CharacterPreferencesPage;
+import com.arcaner.warlock.rcp.prefs.HighlightNamesPreferencePage;
+import com.arcaner.warlock.rcp.prefs.HighlightStringsPreferencePage;
 import com.arcaner.warlock.rcp.prefs.LookAndFeelPreferencePage;
-import com.arcaner.warlock.rcp.prefs.StandardColorsAndFontsPreferencePage;
+import com.arcaner.warlock.rcp.prefs.PresetsPreferencePage;
+import com.arcaner.warlock.rcp.views.GameView;
 
 public class PreferencesHandler extends SimpleCommandHandler
 {
 	
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		
-		PreferenceDialog dialog = PreferencesUtil.createPreferenceDialogOn(Display.getDefault().getActiveShell(),
-				StandardColorsAndFontsPreferencePage.PAGE_ID,
-			new String[] {
-				LookAndFeelPreferencePage.PAGE_ID,
-				StandardColorsAndFontsPreferencePage.PAGE_ID
-			}, null);
-		
-		// remove all non-warlock preference nodes
-		IPreferenceNode[] nodes = dialog.getPreferenceManager().getRootSubNodes();
-		for (IPreferenceNode node : nodes)
+	@Override
+	public boolean isEnabled() {
+		GameView inFocus = GameView.getViewInFocus();
+		if (inFocus != null)
 		{
-			if (!node.getId().startsWith(Warlock2Plugin.PLUGIN_ID))
+			IStormFrontClient client = inFocus.getStormFrontClient();
+			if (client != null)
 			{
-				dialog.getPreferenceManager().remove(node);
+				return client.getCharacterName() != null;
 			}
 		}
+		return false;
+	}
+	
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+		PreferenceDialog dialog = PreferencesUtil.createPropertyDialogOn(Display.getDefault().getActiveShell(),
+				GameView.getViewInFocus(), 
+				HighlightStringsPreferencePage.PAGE_ID,
+				new String[] {
+					CharacterPreferencesPage.PAGE_ID,
+					LookAndFeelPreferencePage.PAGE_ID,
+					PresetsPreferencePage.PAGE_ID,
+					HighlightStringsPreferencePage.PAGE_ID,
+					HighlightNamesPreferencePage.PAGE_ID
+				}, null);
+//		PreferenceDialog dialog = PreferencesUtil.createPreferenceDialogOn(Display.getDefault().getActiveShell(),
+//				PresetsPreferencePage.PAGE_ID,
+//			new String[] {
+//				LookAndFeelPreferencePage.PAGE_ID,
+//				PresetsPreferencePage.PAGE_ID
+//			}, null);
+		
+		// remove all non-warlock preference nodes
+//		IPreferenceNode[] nodes = dialog.getPreferenceManager().getRootSubNodes();
+//		for (IPreferenceNode node : nodes)
+//		{
+//			if (!node.getId().startsWith(Warlock2Plugin.PLUGIN_ID))
+//			{
+//				dialog.getPreferenceManager().remove(node);
+//			}
+//		}
 		
 		int response = dialog.open();
 		
