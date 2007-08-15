0333d5c16348a4b22fb084392bb4345ca1c19f98
Marshall Culpepper <marshall@arcaner.com>
2007-08-15 03:50:09 +0000

    swt wrapper for script listener interface
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@376 bb130017-240f-0410-9613-9c2c827820fb

---
+++ core/cc.warlock.rcp/src/cc/warlock/rcp/ui/client/SWTScriptListener.java
@@ -0,0 +1,85 @@
+package cc.warlock.rcp.ui.client;
+
+import org.eclipse.swt.widgets.Display;
+
+import cc.warlock.script.IScript;
+import cc.warlock.script.IScriptListener;
+
+public class SWTScriptListener implements IScriptListener {
+
+	protected IScriptListener listener;
+	protected ListenerWrapper wrapper;
+	protected boolean asynch;
+	
+	public SWTScriptListener (IScriptListener listener)
+	{
+		this(listener, false);
+	}
+	
+	public SWTScriptListener (IScriptListener listener, boolean asynch)
+	{
+		this.listener = listener;
+		this.wrapper = new ListenerWrapper();
+		this.asynch = asynch;
+	}
+	
+	private static enum EventType {
+		Paused, Resumed, Started, Stopped
+	}
+	
+	private class ListenerWrapper implements Runnable
+	{
+		public IScript script;
+		public EventType eventType;
+		public boolean userStopped;
+		
+		public void run() {
+			switch (eventType)
+			{
+			case Paused: listener.scriptPaused(script); break;
+			case Resumed: listener.scriptResumed(script); break;
+			case Started: listener.scriptStarted(script); break;
+			case Stopped: listener.scriptStopped(script, userStopped); break;
+			}
+			script = null;
+			eventType = null;
+			userStopped = false;
+		}
+	}
+	
+	protected void run(Runnable runnable)
+	{
+		if (asynch)
+		{
+			Display.getDefault().asyncExec(runnable);
+		} else {
+			Display.getDefault().syncExec(runnable);
+		}
+	}
+	
+	public void scriptPaused(IScript script) {
+		wrapper.script = script;
+		wrapper.eventType = EventType.Paused;
+		run(wrapper);
+	}
+
+	public void scriptResumed(IScript script) {
+		wrapper.script = script;
+		wrapper.eventType = EventType.Resumed;
+		run(wrapper);
+	}
+
+	public void scriptStarted(IScript script) {
+		wrapper.script = script;
+		wrapper.eventType = EventType.Started;
+		run(wrapper);
+	}
+
+	public void scriptStopped(IScript script, boolean userStopped) {
+		wrapper.script = script;
+		wrapper.userStopped = userStopped;
+		wrapper.eventType = EventType.Stopped;
+		run(wrapper);
+	}
+
+}
