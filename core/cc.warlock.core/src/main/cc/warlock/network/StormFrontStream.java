f76a5db3500641f81f1ca81af7adf7eea0dc0d87
Sean Proctor <sproctor@gmail.com>
2007-08-16 23:53:43 +0000

    don't remove newlines after empty tags
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@391 bb130017-240f-0410-9613-9c2c827820fb

--- core/cc.warlock.core/src/main/cc/warlock/network/StormFrontStream.java
+++ core/cc.warlock.core/src/main/cc/warlock/network/StormFrontStream.java
@@ -25,7 +25,7 @@ public class StormFrontStream extends InputStream {
 			new Replacer(Pattern.compile("<(?=[^\\w/])"), "&lt;"),
 			//new Replacer(Pattern.compile("(?<=^[^<]+)>"), "&gt;"),
 			new Replacer(Pattern.compile("\07"), ""),
-			new Replacer(Pattern.compile(">(\\n|\\r\\n)"), ">"),
+			new Replacer(Pattern.compile("(?<!/)>(\\n|\\r\\n)"), ">"),
 			new Replacer(Pattern.compile("\\w+=\"[^\"]*(\")[^=]*\"(\\s+\\w+=|\\s*/>)"), "&quot;", 1),
 			};
 	private CharSequence buffer = null;
