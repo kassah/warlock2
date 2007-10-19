5c16783a77b6ffe2b14576fdb169209b50626647
William Lightning <kassah@gmail.com>
2007-10-19 19:21:02 +0000

    Adding Dynamic Menus for UserStreams
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@973 bb130017-240f-0410-9613-9c2c827820fb

---
+++ core/cc.warlock.rcp.userstreams/src/cc/warlock/rcp/userstreams/ui/actions/StreamShowAction.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package cc.warlock.rcp.userstreams.ui.actions;
+
+import org.eclipse.jface.action.Action;
+
+import cc.warlock.core.configuration.Profile;
+
+/**
+ * @author Will Robertson
+ *
+ */
+public class StreamShowAction extends Action {
+	public StreamShowAction () {
+		//super(profile.getCharacterName(), StormFrontSharedImages.getImageDescriptor(StormFrontSharedImages.IMG_CHARACTER));
+		//setDescription(profile.getGameName() + " character \"" + profile.getCharacterName() + "\"");
+		
+		//this.profile = profile;
+	}
+}
---
+++ core/cc.warlock.rcp.userstreams/src/cc/warlock/rcp/userstreams/ui/menu/StreamsContributionItem.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package cc.warlock.rcp.userstreams.ui.menu;
+
+import org.eclipse.jface.action.ActionContributionItem;
+import org.eclipse.jface.action.IContributionItem;
+import org.eclipse.ui.actions.CompoundContributionItem;
+
+import cc.warlock.rcp.userstreams.ui.actions.StreamShowAction;
+
+/**
+ * @author Will Robertson
+ *
+ */
+public class StreamsContributionItem extends CompoundContributionItem  {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.actions.CompoundContributionItem#getContributionItems()
+	 */
+	@Override
+	protected IContributionItem[] getContributionItems() {
+		// TODO Auto-generated method stub
+		//return null;
+		//Collection<Profile> profiles = SavedProfiles.getAllProfiles();
+		IContributionItem[] items = new IContributionItem[1]; //profiles.size()];
+		//int i = 0;
+		
+		items[1] = new ActionContributionItem(new StreamShowAction());
+		//for (Profile profile : profiles)
+		//{
+		//	items[i] = new ActionContributionItem(new ProfileConnectAction(profile));
+		//	i++;
+		//}
+		
+		return items; 
+	}
+
+}
