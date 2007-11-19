b2013b7c9ac1fd397a6dc7987ce064a17cdaf4cd
Sean Proctor <sproctor@gmail.com>
2007-11-19 02:10:55 +0000

    remove seemingly random @Override
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@1098 bb130017-240f-0410-9613-9c2c827820fb

--- stormfront/cc.warlock.core.stormfront/src/main/cc/warlock/core/stormfront/serversettings/server/ServerScriptProvider.java
+++ stormfront/cc.warlock.core.stormfront/src/main/cc/warlock/core/stormfront/serversettings/server/ServerScriptProvider.java
@@ -34,7 +34,6 @@ public class ServerScriptProvider implements IServerScriptProvider
 			return script.getComment();
 		}
 		
-		@Override
 		public ServerScript getServerScript() {
 			return script;
 		}
