70bf3b0731f7504b716e6e49015ad5c502785ded
Sean Proctor <sproctor@gmail.com>
2007-11-19 04:18:44 +0000

    don't buffer the debug view (or any other StormFrontConnection listeners)
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@1103 bb130017-240f-0410-9613-9c2c827820fb

--- stormfront/cc.warlock.core.stormfront/src/main/cc/warlock/core/stormfront/network/StormFrontReader.java
+++ stormfront/cc.warlock.core.stormfront/src/main/cc/warlock/core/stormfront/network/StormFrontReader.java
@@ -7,7 +7,6 @@ import java.nio.CharBuffer;
 
 public class StormFrontReader extends BufferedReader {
 
-	protected StringBuffer currentLine = new StringBuffer();
 	protected StormFrontConnection connection;
 	
 	public StormFrontReader (StormFrontConnection connection, Reader reader)
@@ -20,36 +19,15 @@ public class StormFrontReader extends BufferedReader {
 		int c = super.read();
 		if (c != -1)
 		{
-			currentLine.append((char)c);
-			if (((char)c) == '\n')
-			{
-				connection.dataReady(currentLine.toString());
-				currentLine.setLength(0);
-			}
+			connection.dataReady(String.valueOf(c));
 		}
 		return c;
-	}
-
-	private void appendChars (char[] cbuf, int start, int bytesRead)
-	{
-		if (bytesRead > 0)
-		{
-			currentLine.append(cbuf, start, bytesRead);
-			int newLineIndex = currentLine.indexOf("\n");
-			while (newLineIndex != -1)
-			{
-				String line = currentLine.substring(0, newLineIndex);
-				connection.dataReady(line);
-				currentLine.replace(0, newLineIndex+1, "");
-				newLineIndex = currentLine.indexOf("\n");
-			}
-		}
 	}
 	
 	@Override
 	public int read(char[] cbuf) throws IOException {
 		int bytesRead = super.read(cbuf);
-		appendChars(cbuf, 0, bytesRead);
+		connection.dataReady(String.valueOf(cbuf));
 		
 		return bytesRead;
 	}
@@ -57,7 +35,7 @@ public class StormFrontReader extends BufferedReader {
 	@Override
 	public int read(char[] cbuf, int off, int len) throws IOException {
 		int bytesRead = super.read(cbuf, off, len);
-		appendChars(cbuf, off, bytesRead);
+		connection.dataReady(String.valueOf(cbuf, off, bytesRead));
 		
 		return bytesRead;
 	}
@@ -65,7 +43,7 @@ public class StormFrontReader extends BufferedReader {
 	@Override
 	public int read(CharBuffer target) throws IOException {
 		int bytesRead = super.read(target);
-		appendChars(target.array(), 0, bytesRead);
+		connection.dataReady(target.toString());
 		
 		return bytesRead;
 	}
