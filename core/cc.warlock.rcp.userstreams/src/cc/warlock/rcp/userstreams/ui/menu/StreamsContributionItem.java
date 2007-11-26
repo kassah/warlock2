47d7e376e78aa96d1f8ea80ba1032819053e6713
Marshall Culpepper <marshall.law@gmail.com>
2007-11-26 23:38:59 +0000

    some code cleanup to make things look nicer/easier to find
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@1146 bb130017-240f-0410-9613-9c2c827820fb

--- core/cc.warlock.rcp.userstreams/src/cc/warlock/rcp/userstreams/ui/menu/StreamsContributionItem.java
+++ core/cc.warlock.rcp.userstreams/src/cc/warlock/rcp/userstreams/ui/menu/StreamsContributionItem.java
@@ -3,6 +3,8 @@
  */
 package cc.warlock.rcp.userstreams.ui.menu;
 
+import java.util.ArrayList;
+
 import org.eclipse.jface.action.ActionContributionItem;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.action.IContributionItem;
@@ -18,40 +20,52 @@ import cc.warlock.rcp.userstreams.ui.actions.StreamShowAction;
  */
 public class StreamsContributionItem extends CompoundContributionItem  {
 
+	protected ArrayList<IStreamFilter> getEventsFilters ()
+	{
+		ArrayList<IStreamFilter> filters = new ArrayList<IStreamFilter>();
+		filters.add(new StreamFilter("^You've gained a new rank in .+\\.", IStreamFilter.type.regex));
+		filters.add(new StreamFilter("^Announcement: .+$", IStreamFilter.type.regex));
+		filters.add(new StreamFilter("^System Announcement: .+$", IStreamFilter.type.regex));
+		filters.add(new StreamFilter("^(Xibar|Katamba|Yavash) slowly rises above the horizon\\.", IStreamFilter.type.regex));
+		filters.add(new StreamFilter("^(Xibar|Katamba|Yavash) sets, slowly dropping below the horizon\\.", IStreamFilter.type.regex));
+		
+		return filters;
+	}
+	
+	protected ArrayList<IStreamFilter> getConversationsFilters ()
+	{
+		ArrayList<IStreamFilter> filters = new ArrayList<IStreamFilter>();
+		// (say|ask|exclaim|whisper)
+		filters.add(new StreamFilter("^\\w+ \\w+( (at|to) \\w+)?, \".+\"$", IStreamFilter.type.regex));
+		filters.add(new StreamFilter("thoughts in your head", IStreamFilter.type.string));
+		filters.add(new StreamFilter("^\\w+ (nod|lean|stretch|smile|yawn|chuckle|chortle|grin|beam|hug|applaud|babble|blink|bow|cackle|cringe|cower|weep|mumble|wave|ponder|peers quizzically|snort|snuggle|cuddle|smirk|laugh|jumps back from|nods? slightly|whistles? a merry tune.)", IStreamFilter.type.regex));
+		filters.add(new StreamFilter("accusatory", IStreamFilter.type.string));
+		filters.add(new StreamFilter("flush", IStreamFilter.type.string));
+		filters.add(new StreamFilter("\"Boo\"", IStreamFilter.type.string));
+		filters.add(new StreamFilter("weep", IStreamFilter.type.string));
+		filters.add(new StreamFilter("mumble", IStreamFilter.type.string));
+		filters.add(new StreamFilter("dance", IStreamFilter.type.string));
+		filters.add(new StreamFilter("tickle", IStreamFilter.type.string));
+		filters.add(new StreamFilter("^\\(.+\\)$", IStreamFilter.type.regex));
+		
+		return filters;
+	}
+
+	protected IContributionItem createStreamContributionItem (String name, ArrayList<IStreamFilter> filters)
+	{
+		return new ActionContributionItem(new StreamShowAction(name, filters.toArray(new IStreamFilter[filters.size()])));
+	}
+	
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.actions.CompoundContributionItem#getContributionItems()
 	 */
 	@Override
 	protected IContributionItem[] getContributionItems() {
 		// Add Menu Items
-		IContributionItem[] items = new IContributionItem[2];
-		IAction item;
-		IStreamFilter[] filters = new IStreamFilter[5];
-		filters[0] = new StreamFilter("^You've gained a new rank in .+\\.", IStreamFilter.type.regex);
-		filters[1] = new StreamFilter("^Announcement: .+$", IStreamFilter.type.regex);
-		filters[2] = new StreamFilter("^System Announcement: .+$", IStreamFilter.type.regex);
-		filters[3] = new StreamFilter("^(Xibar|Katamba|Yavash) slowly rises above the horizon\\.", IStreamFilter.type.regex);
-		filters[4] = new StreamFilter("^(Xibar|Katamba|Yavash) sets, slowly dropping below the horizon\\.", IStreamFilter.type.regex);
-		item = new StreamShowAction("Events", filters);
-		items[0] = new ActionContributionItem(item);
-		filters = new IStreamFilter[11];
-		int i = -1;
-		// (say|ask|exclaim|whisper)
-		filters[++i] = new StreamFilter("^\\w+ \\w+( (at|to) \\w+)?, \".+\"$", IStreamFilter.type.regex);
-		filters[++i] = new StreamFilter("thoughts in your head", IStreamFilter.type.string);
-		filters[++i] = new StreamFilter("^\\w+ (nod|lean|stretch|smile|yawn|chuckle|chortle|grin|beam|hug|applaud|babble|blink|bow|cackle|cringe|cower|weep|mumble|wave|ponder|peers quizzically|snort|snuggle|cuddle|smirk|laugh|jumps back from|nods? slightly|whistles? a merry tune.)", IStreamFilter.type.regex);
-		filters[++i] = new StreamFilter("accusatory", IStreamFilter.type.string);
-		filters[++i] = new StreamFilter("flush", IStreamFilter.type.string);
-		filters[++i] = new StreamFilter("\"Boo\"", IStreamFilter.type.string);
-		filters[++i] = new StreamFilter("weep", IStreamFilter.type.string);
-		filters[++i] = new StreamFilter("mumble", IStreamFilter.type.string);
-		filters[++i] = new StreamFilter("dance", IStreamFilter.type.string);
-		filters[++i] = new StreamFilter("tickle", IStreamFilter.type.string);
-		filters[++i] = new StreamFilter("^\\(.+\\)$", IStreamFilter.type.regex);
-		item = new StreamShowAction("Conversations", filters);
-		items[1] = new ActionContributionItem(item);
+		ArrayList<IContributionItem> items = new ArrayList<IContributionItem>();
+		items.add(createStreamContributionItem("Events", getEventsFilters()));
+		items.add(createStreamContributionItem("Conversations", getConversationsFilters()));
 		
-		return items; 
+		return items.toArray(new IContributionItem[items.size()]); 
 	}
-
 }
