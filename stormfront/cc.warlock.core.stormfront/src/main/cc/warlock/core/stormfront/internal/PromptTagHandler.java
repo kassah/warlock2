1d4168e3819de4523037e1785c51b72eebf4fe7a
Sean Proctor <sproctor@gmail.com>
2007-11-27 17:30:42 +0000

    forgot to update imports with the last change
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@1150 bb130017-240f-0410-9613-9c2c827820fb

--- stormfront/cc.warlock.core.stormfront/src/main/cc/warlock/core/stormfront/internal/PromptTagHandler.java
+++ stormfront/cc.warlock.core.stormfront/src/main/cc/warlock/core/stormfront/internal/PromptTagHandler.java
@@ -4,7 +4,6 @@
 package cc.warlock.core.stormfront.internal;
 
 import cc.warlock.core.stormfront.IStormFrontProtocolHandler;
-import cc.warlock.core.stormfront.client.IStormFrontClient;
 import cc.warlock.core.stormfront.xml.StormFrontAttributeList;
 
 
