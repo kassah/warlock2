c82cb18c0ee626a9df6cc8ba10b4234a6c16c02c
Sean Proctor <sproctor@gmail.com>
2007-12-12 22:23:13 +0000

    added a menu item to open the debug view
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@1296 bb130017-240f-0410-9613-9c2c827820fb

--- stormfront/cc.warlock.rcp.stormfront/src/cc/warlock/rcp/stormfront/ui/menu/StreamWindowContributionItem.java
+++ stormfront/cc.warlock.rcp.stormfront/src/cc/warlock/rcp/stormfront/ui/menu/StreamWindowContributionItem.java
@@ -1,24 +1,60 @@
 package cc.warlock.rcp.stormfront.ui.menu;
 
+import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.ActionContributionItem;
 import org.eclipse.jface.action.IContributionItem;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.CompoundContributionItem;
 
+import cc.warlock.core.client.IWarlockClient;
 import cc.warlock.core.stormfront.client.IStormFrontClient;
+import cc.warlock.core.stormfront.network.StormFrontConnection;
 import cc.warlock.rcp.actions.OpenStreamWindowAction;
+import cc.warlock.rcp.plugin.Warlock2Plugin;
+import cc.warlock.rcp.views.DebugView;
 import cc.warlock.rcp.views.StreamView;
 
 
 public class StreamWindowContributionItem extends CompoundContributionItem {
 
+	private class DebugAction extends Action {
+		
+		private static final String title = "Debug View";
+		
+		public DebugAction() {
+			super(title, Action.AS_CHECK_BOX);
+		}
+		
+		public void run() {
+			IWarlockClient client = Warlock2Plugin.getDefault().getCurrentClient();
+			StormFrontConnection connection = (StormFrontConnection)client.getConnection();
+			
+			try {
+				DebugView view = (DebugView)
+				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(DebugView.VIEW_ID, connection.getKey(), IWorkbenchPage.VIEW_VISIBLE);
+				view.setClient(client);
+				view.setConnection(client.getConnection());
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		@Override
+		public String getText() {
+	 		return title;
+		}
+	}
+	
 	@Override
 	protected IContributionItem[] getContributionItems() {	
-		IContributionItem[] items = new IContributionItem[5];
+		IContributionItem[] items = new IContributionItem[6];
 		items[0] = streamContribution("Thoughts", IStormFrontClient.THOUGHTS_STREAM_NAME, StreamView.TOP_STREAM_PREFIX);
 		items[1] = streamContribution("Inventory", IStormFrontClient.INVENTORY_STREAM_NAME, StreamView.RIGHT_STREAM_PREFIX);
 		items[2] = streamContribution("Deaths", IStormFrontClient.DEATH_STREAM_NAME, StreamView.TOP_STREAM_PREFIX);
 		items[3] = streamContribution("Current Room", IStormFrontClient.ROOM_STREAM_NAME, StreamView.RIGHT_STREAM_PREFIX);
 		items[4] = streamContribution("Familiar", IStormFrontClient.FAMILIAR_STREAM_NAME, StreamView.RIGHT_STREAM_PREFIX);
+		items[5] = new ActionContributionItem(new DebugAction());
 		return items;
 	}
 	
