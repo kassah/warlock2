87379be9cce53de78466118ccaa1c42bf46ec20a
Sean Proctor <sproctor@gmail.com>
2008-01-05 23:54:37 +0000

    fix leading/trailing spaces being put in regex from actions. put variables from action matches into local variables.
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@1398 bb130017-240f-0410-9613-9c2c827820fb

--- stormfront/cc.warlock.core.stormfront.script/src/main/cc/warlock/core/stormfront/script/wsl/WSLAction.java
+++ stormfront/cc.warlock.core.stormfront.script/src/main/cc/warlock/core/stormfront/script/wsl/WSLAction.java
@@ -1,10 +1,14 @@
 package cc.warlock.core.stormfront.script.wsl;
 
+import cc.warlock.core.script.IMatch;
+import cc.warlock.core.script.internal.RegexMatch;
+
 public class WSLAction extends WSLAbstractCommand {
 
 	private WSLScript script;
 	private WSLAbstractCommand command;
 	private IWSLValue when;
+	private IMatch match;
 	
 	public WSLAction(int lineNum, WSLScript script, WSLAbstractCommand command, IWSLValue when) {
 		super(lineNum);
@@ -14,19 +18,16 @@ public class WSLAction extends WSLAbstractCommand {
 	}
 	
 	private class WSLActionAdapter implements Runnable {
-		private WSLAbstractCommand command;
-		
-		public WSLActionAdapter(WSLAbstractCommand command) {
-			this.command = command;
-		}
 		
 		public void run() {
+			if(match instanceof RegexMatch)
+				script.setVariablesFromMatch((RegexMatch)match);
 			command.execute();
 		}
 	}
 	
 	public void execute() {
-		script.scriptCommands.addAction(new WSLActionAdapter(command), when.toString());
+		match = script.scriptCommands.addAction(new WSLActionAdapter(), when.toString().trim());
 	}
 
 }
