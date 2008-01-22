5623537dd205df3564d3791730b4374e70815818
Sean Proctor <sproctor@gmail.com>
2008-01-22 01:11:58 +0000

    was omitted from last commit due to some stupidity over here.
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@1442 bb130017-240f-0410-9613-9c2c827820fb

--- core/cc.warlock.core/src/main/cc/warlock/core/client/internal/Compass.java
+++ core/cc.warlock.core/src/main/cc/warlock/core/client/internal/Compass.java
@@ -1,83 +1,20 @@
-/*
- * Created on Jan 15, 2005
- */
 package cc.warlock.core.client.internal;
 
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 import cc.warlock.core.client.ICompass;
-import cc.warlock.core.client.IPropertyListener;
-import cc.warlock.core.client.IWarlockClient;
 
+public class Compass implements ICompass {
 
-/**
- * @author Marshall
- */
-public class Compass extends ClientProperty<String> implements ICompass {
+	private HashSet<DirectionType> dirs = new HashSet<DirectionType>();
 	
-	protected Map<DirectionType, Boolean> compass;
-	protected static final HashMap<String, DirectionType> directions = new HashMap<String, DirectionType>();
-	
-	static {
-		for (DirectionType direction : DirectionType.values())
-		{
-			if (direction != DirectionType.None)
-			{
-				directions.put(direction.getName(), direction);
-				directions.put(direction.getShortName(), direction);
-			}
-		}
+	public boolean addDirection(DirectionType dir) {
+		return dirs.add(dir);
 	}
 	
-	public Compass(IWarlockClient client) {
-		super(client, "compass", null);
-		
-		compass = new Hashtable<DirectionType, Boolean>();
-		
-		for(DirectionType type : DirectionType.values())
-		{
-			compass.put(type, false);
-		}		
-	}
-	
-	public Map<DirectionType, Boolean> getDirections() {
-		return compass;
+	public Set<DirectionType> getDirections() {
+		return dirs;
 	}
 
-	public void clear() {
-		for (DirectionType type : DirectionType.values())
-		{
-			compass.put(type, false);
-		}
-		
-		for (IPropertyListener<String> listener : listeners) {
-			listener.propertyCleared(this, null);
-		}
-	}
-
-	public void set(String data) {
-		// We do not notify on set, only when the compass is finished.
-		DirectionType direction = directions.get(data);
-		
-		if(direction == null)
-			return;
-		
-		compass.put(direction, true);
-		for (IPropertyListener<String> listener : listeners)
-		{
-			listener.propertyChanged(this, null);
-		}
-	}
-	
-	public void activate() {
-		for(IPropertyListener<String> listener : listeners) {
-			listener.propertyActivated(this);
-		}
-	}
-	
-	public static DirectionType getValue(String direction) {
-		return directions.get(direction);
-	}
 }
