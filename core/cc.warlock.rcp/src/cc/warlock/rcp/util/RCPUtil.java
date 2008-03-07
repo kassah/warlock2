8cd1a88570fbc400c2003f752b7419930d00001f
Marshall Culpepper <marshall.law@gmail.com>
2008-03-07 20:07:02 +0000

    some working audio playing implementation code. we should be able to add audio alerts etc now.
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@1689 bb130017-240f-0410-9613-9c2c827820fb

--- core/cc.warlock.rcp/src/cc/warlock/rcp/util/RCPUtil.java
+++ core/cc.warlock.rcp/src/cc/warlock/rcp/util/RCPUtil.java
@@ -21,9 +21,23 @@
  */
 package cc.warlock.rcp.util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineListener;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IPerspectiveDescriptor;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
@@ -64,5 +78,59 @@ public class RCPUtil {
 		double pixels = pointSize * (96.0/72.0);
 		
 		return (int) Math.round(pixels);
+	}
+	
+	private static class Flag {
+		public boolean value;
+	}
+	
+	public static void playSound (File soundFile)
+	{
+		try {
+			Clip clip = AudioSystem.getClip();
+			
+			AudioFileFormat format = AudioSystem.getAudioFileFormat(soundFile);
+			
+			final Flag finished = new Flag();
+			finished.value = false;
+			final AudioInputStream stream = new AudioInputStream(new FileInputStream(soundFile), format.getFormat(), format.getFrameLength()); 
+			clip.open(stream);
+			clip.addLineListener(new LineListener() {
+				public void update(LineEvent event) {
+					if (event.getType() == LineEvent.Type.STOP) {
+						try {
+							stream.close();
+							finished.value = true;
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+			});
+			clip.start();
+			
+			while (!finished.value) {
+				if (Display.getDefault() != null) {
+					Display.getDefault().readAndDispatch();
+				} else {
+					Thread.sleep((long)300);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedAudioFileException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
