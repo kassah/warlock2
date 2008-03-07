83bafa6c0762f7e4f38ce6e72a319d25467b6a59
Marshall Culpepper <marshall.law@gmail.com>
2008-03-07 20:04:57 +0000

    warlock preference page actually has a lot of useful utility methods now, and should probably be added to / extended by all pref pages
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@1687 bb130017-240f-0410-9613-9c2c827820fb

--- core/cc.warlock.rcp/src/cc/warlock/rcp/prefs/WarlockPreferencePage.java
+++ core/cc.warlock.rcp/src/cc/warlock/rcp/prefs/WarlockPreferencePage.java
@@ -21,21 +21,115 @@
  */
 package cc.warlock.rcp.prefs;
 
+import org.eclipse.jface.preference.ColorSelector;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.IWorkbenchPropertyPage;
 import org.eclipse.ui.dialogs.PropertyPage;
 
-public class WarlockPreferencePage extends PropertyPage implements
-		IWorkbenchPropertyPage {
+import cc.warlock.rcp.util.FontSelector;
 
-
-	@Override
-	protected Control createContents(Composite parent) {
-		Composite main = new Composite(parent, SWT.NONE);
+public abstract class WarlockPreferencePage extends PropertyPage implements
+		IWorkbenchPropertyPage, SelectionListener, IPropertyChangeListener {
+	
+	public Button createButton(Composite parent, int flags)
+	{
+		Button button = new Button(parent, flags);
+		button.addSelectionListener(this);
 		
-		return main;
+		return button;
+	}
+	
+	public Button createButton(Composite parent, String text, int flags)
+	{
+		Button button = createButton(parent, flags);
+		button.setText(text);
+		
+		return button;
+	}
+	
+	public Button createButton(Composite parent)
+	{
+		return createButton(parent, SWT.PUSH);
+	}
+	
+	public Button createButton(Composite parent, String text)
+	{
+		return createButton(parent, text, SWT.PUSH);
+	}
+	
+	public Button createCheckbox(Composite parent)
+	{
+		return createButton(parent, SWT.CHECK);
+	}
+	
+	public Button createCheckbox(Composite parent, String text)
+	{
+		return createButton(parent, text, SWT.CHECK);
+	}
+	
+	public Button createRadio(Composite parent)
+	{
+		return createButton(parent, SWT.RADIO);
+	}
+	
+	public Button createRadio(Composite parent, String text)
+	{
+		return createButton(parent, text, SWT.RADIO);
 	}
 
+	public ColorSelector createColorSelector (Composite parent)
+	{
+		ColorSelector selector = new ColorSelector(parent);
+		selector.addListener(this);
+		
+		return selector;
+	}
+	
+	public FontSelector createFontSelector (Composite parent)
+	{
+		FontSelector selector = new FontSelector(parent);
+		selector.addListener(this);
+		
+		return selector;
+	}
+	
+	public void widgetDefaultSelected(SelectionEvent e) {
+		widgetSelected(e);
+	}
+	
+	public void widgetSelected(SelectionEvent e) {
+		if (e.getSource() instanceof Button)
+		{
+			buttonPressed((Button)e.getSource());
+		}
+	}
+	
+	protected void buttonPressed(Button button) {
+		
+	}
+	
+	public void propertyChange(PropertyChangeEvent event) {
+		if (event.getSource() instanceof ColorSelector)
+		{
+			colorSelectorChanged((ColorSelector)event.getSource());
+		}
+		else if (event.getSource() instanceof FontSelector)
+		{
+			fontSelectorChanged((FontSelector)event.getSource());
+		}
+	}
+	
+	protected void colorSelectorChanged(ColorSelector selector) {
+		
+	}
+	
+	protected void fontSelectorChanged(FontSelector selector) {
+		
+	}
 }
