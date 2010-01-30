c940fc284c92785441cd314fe07f9738590abacd
Sean Proctor <sproctor@gmail.com>
2010-01-30 20:18:02 +0000

    remove interface @Overrides
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@2197 bb130017-240f-0410-9613-9c2c827820fb

--- core/cc.warlock.core/src/main/cc/warlock/core/client/internal/WarlockClientListener.java
+++ core/cc.warlock.core/src/main/cc/warlock/core/client/internal/WarlockClientListener.java
@@ -5,19 +5,14 @@ import cc.warlock.core.client.IWarlockClientListener;
 
 public abstract class WarlockClientListener implements IWarlockClientListener {
 
-	@Override
 	public abstract void clientActivated(IWarlockClient client);
 
-	@Override
 	public abstract void clientConnected(IWarlockClient client);
 
-	@Override
 	public abstract void clientDisconnected(IWarlockClient client);
 
-	@Override
 	public abstract void clientRemoved(IWarlockClient client);
 
-	@Override
 	public abstract void clientSettingsLoaded(IWarlockClient client);
 
 }
