7ae3c9215e862f9037f5be25bda9a8b8f1acc403
William Lightning <kassah@gmail.com>
2010-03-05 08:08:37 +0000

    Make WarlockPerferenceArrayProvider allow for getAll in a safe way. Changed some references from highlight to value to better reflect it's generic quality.
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@2292 bb130017-240f-0410-9613-9c2c827820fb

--- core/cc.warlock.core/src/main/cc/warlock/core/client/settings/WarlockPreferenceArrayProvider.java
+++ core/cc.warlock.core/src/main/cc/warlock/core/client/settings/WarlockPreferenceArrayProvider.java
@@ -1,25 +1,53 @@
 package cc.warlock.core.client.settings;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.osgi.service.prefs.BackingStoreException;
 import org.osgi.service.prefs.Preferences;
 
 public abstract class WarlockPreferenceArrayProvider<T> extends WarlockPreferenceProvider<T> {
 	
-	public String add(WarlockClientPreferences prefs, T highlight) {
+	public String add(WarlockPreferences prefs, T value) {
 		Preferences hnode = prefs.getNode().node(getNodeName());
 		
 		int max = 0;
 		try {
 			for (String name : hnode.childrenNames()) {
-				int cur = Integer.parseInt(name);
-				if(cur > max)
-					max = cur;
+				// Wrap this, so if we encounter non-numeric keys, we'll just let them pass through
+				try {
+					int cur = Integer.parseInt(name);
+					if(cur > max)
+						max = cur;
+				} catch (NumberFormatException e) {
+					// Nothing
+				}
 			}
 		} catch(BackingStoreException e) {
 			e.printStackTrace();
 		}
 		String name = Integer.toString(max + 1);
-		set(hnode.node(name), highlight);
+		set(hnode.node(name), value);
 		return name;
+	}
+	
+	@Override
+	public Collection<T> getAll(WarlockPreferences prefs) {
+		Preferences hnode = prefs.getNode().node(getNodeName());
+		ArrayList<T> arr = new ArrayList<T>();
+		try {
+			for (String name : hnode.childrenNames()) {
+				// Wrap this, so if we encounter non-numeric keys, we'll just let them pass through
+				try {
+					Integer.parseInt(name); // This will throw an exception if it's a not numeric.
+					arr.add(this.get(hnode.node(name)));
+				} catch (NumberFormatException e) {
+					// Nothing
+				}
+			}
+		} catch(BackingStoreException e) {
+			e.printStackTrace();
+		}
+		return arr;
 	}
 }
