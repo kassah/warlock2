ee23cd3655bf6849354a1db83d3fdcc5436c16a0
William Lightning <kassah@gmail.com>
2010-03-05 08:19:16 +0000

    Remove some SF specific crap from generic warlock core Profile
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@2295 bb130017-240f-0410-9613-9c2c827820fb

--- core/cc.warlock.core/src/main/cc/warlock/core/profile/Profile.java
+++ core/cc.warlock.core/src/main/cc/warlock/core/profile/Profile.java
@@ -29,82 +29,27 @@ package cc.warlock.core.profile;
  * @author Marshall
  */
 public class Profile {
-
-	protected String id, name, gameCode, gameName;
-	protected Account account;
+	private String name;
 	
 	public Profile () { }
-	public Profile (Account account, String id, String name, String gameCode, String gameName)
+	public Profile (String name)
 	{
-		this(id, name, gameCode, gameName);
-		
-		this.account = account;
-		if (account != null && !account.getProfiles().contains(this)) {
-			account.getProfiles().add(this);
-		}
+		this.setName(name);
 	}
 	
-	public Profile (String id, String name, String gameCode, String gameName)
-	{
-		this.id = id;
-		this.name = name;
-		this.gameCode = gameCode;
-		this.gameName = gameName;
-	}
-	
-	public Profile (Account account, Profile other)
-	{
-		this.id = other.id == null ? null : new String(other.id);
-		this.name = other.name== null ? null : new String(other.name);
-		this.gameCode = other.gameCode == null ? null : new String(other.gameCode);
-		this.gameName = other.gameName == null ? null : new String(other.gameName);
-		
-		this.account = account;
-		if (account != null && !account.getProfiles().contains(this)) {
-			account.getProfiles().add(this);
-		}
-	}
-	
-	/**
-	 * @return Returns the gameCode.
-	 */
-	public String getGameCode() {
-		return gameCode;
+	public Profile (Profile other) {
+		this.setName(other.getName()== null ? null : new String(other.getName()));
 	}
 	/**
-	 * @param gameCode The gameCode to set.
+	 * @param name the name to set
 	 */
-	public void setGameCode(String gameCode) {
-		this.gameCode = gameCode;
-	}
-	/**
-	 * @return Returns the gameName.
-	 */
-	public String getGameName() {
-		return gameName;
-	}
-	/**
-	 * @param gameName The gameName to set.
-	 */
-	public void setGameName(String gameName) {
-		this.gameName = gameName;
-	}
-	public Account getAccount() {
-		return account;
-	}
-	public void setAccount(Account account) {
-		this.account = account;
-	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
 	}
 }
