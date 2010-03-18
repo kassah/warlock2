9cd48330e9de07d713b08abc1c7ffa838c6291b7
William Lightning <kassah@gmail.com>
2010-03-18 01:10:11 +0000

    Add mutter and grunt to the conversations matching list.
    git-svn-id: http://svn.warlock.cc/svn/warlock/warlock2/trunk@2313 bb130017-240f-0410-9613-9c2c827820fb

--- core/cc.warlock.rcp/src/cc/warlock/rcp/views/UserStream.java
+++ core/cc.warlock.rcp/src/cc/warlock/rcp/views/UserStream.java
@@ -141,7 +141,7 @@ public class UserStream extends StreamView {
 	{
 		ArrayList<IStreamFilter> filters = new ArrayList<IStreamFilter>();
 		filters.add(new StreamFilter("\\bthoughts in your head\\b", IStreamFilter.type.regex));
-		filters.add(new StreamFilter("^\\w+ (nod|lean|stretch|smile|yawn|chuckle|chortle|beam|hug|applaud|babble|blink|bow|cackle|cringe|cower|weep|mumble|wave|ponder|peers quizzically|snort|snuggle|cuddle|smirk|laugh|grumble|dance|grin)s?( (at|to|with) \\w+)?\\.$", IStreamFilter.type.regex));
+		filters.add(new StreamFilter("^\\w+ (nod|lean|stretch|smile|yawn|chuckle|chortle|beam|hug|applaud|babble|blink|bow|cackle|cringe|cower|weep|mumble|wave|ponder|peers quizzically|snort|snuggle|cuddle|smirk|laugh|grumble|dance|grin|grunt|mutter)s?( (at|to|with) \\w+)?\\.$", IStreamFilter.type.regex));
 		filters.add(new StreamFilter("^\\((?!You |Your |Type |ASK |To |Invalid |Roundtime: |You're ).+\\)$", IStreamFilter.type.regex));	//act
 		filters.add(new StreamFilter("^SEND\\[\\w+\\].*$", IStreamFilter.type.regex));	// GM sends
 		filters.add(new StreamFilter("^(You belt out, .*|You hear \\w+ yell, .*|\\w+ yells, )$", IStreamFilter.type.regex));	// Yells
