fee57e4128977061a9fe57d2fd8e004dca4e004b
William Lightning <kassah@gmail.com>
2010-03-27 12:21:35 -0700

    Add method to StormFrontAccountProvider to getAccount by name.

--- stormfront/cc.warlock.core.stormfront/src/main/cc/warlock/core/stormfront/preferences/StormFrontAccountProvider.java
+++ stormfront/cc.warlock.core.stormfront/src/main/cc/warlock/core/stormfront/preferences/StormFrontAccountProvider.java
@@ -1,5 +1,7 @@
 package cc.warlock.core.stormfront.preferences;
 
+import java.util.Collection;
+
 import org.osgi.service.prefs.Preferences;
 
 import cc.warlock.core.client.settings.WarlockPreferenceArrayProvider;
@@ -37,5 +39,22 @@ public class StormFrontAccountProvider
 		// TODO Auto-generated method stub
 		node.put("accountName", value.getAccountName());
 		node.put("password", value.getPassword());
+	}
+	
+	/**
+	 * Look up account by account name.
+	 * 
+	 * @param prefs StormFrontPreferences object to read off of.
+	 * @param accountname Name of Account
+	 * @return the specified account, null if not found
+	 */
+	public StormFrontAccount getAccount(StormFrontPreferences prefs, String accountname) {
+		for (StormFrontAccount acct: this.getAll(prefs)) {
+			if (acct.getAccountName().equals(accountname)) {
+				return acct;
+			}
+		}
+		// No such account
+		return null;
 	}
 }
