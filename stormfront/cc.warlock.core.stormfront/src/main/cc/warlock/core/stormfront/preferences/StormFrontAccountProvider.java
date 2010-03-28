dfc4da267b4644310b831bbc468ed8741dc8d6d9
William Lightning <kassah@gmail.com>
2010-03-28 13:14:48 -0700

    StormFrontAccountProvider is no longer an ArrayProvider..

--- stormfront/cc.warlock.core.stormfront/src/main/cc/warlock/core/stormfront/preferences/StormFrontAccountProvider.java
+++ stormfront/cc.warlock.core.stormfront/src/main/cc/warlock/core/stormfront/preferences/StormFrontAccountProvider.java
@@ -1,14 +1,12 @@
 package cc.warlock.core.stormfront.preferences;
 
-import java.util.Collection;
-
 import org.osgi.service.prefs.Preferences;
 
-import cc.warlock.core.client.settings.WarlockPreferenceArrayProvider;
+import cc.warlock.core.client.settings.WarlockPreferenceProvider;
 import cc.warlock.core.stormfront.profile.StormFrontAccount;
 
 public class StormFrontAccountProvider
-	extends WarlockPreferenceArrayProvider<StormFrontAccount> {
+	extends WarlockPreferenceProvider<StormFrontAccount> {
 
 	private static final StormFrontAccountProvider instance =
 		new StormFrontAccountProvider();
@@ -39,22 +37,5 @@ public class StormFrontAccountProvider
 		// TODO Auto-generated method stub
 		node.put("accountName", value.getAccountName());
 		node.put("password", value.getPassword());
-	}
-	
-	/**
-	 * Look up account by account name.
-	 * 
-	 * @param prefs StormFrontPreferences object to read off of.
-	 * @param accountname Name of Account
-	 * @return the specified account, null if not found
-	 */
-	public StormFrontAccount getAccount(StormFrontPreferences prefs, String accountname) {
-		for (StormFrontAccount acct: this.getAll(prefs)) {
-			if (acct.getAccountName().equals(accountname)) {
-				return acct;
-			}
-		}
-		// No such account
-		return null;
 	}
 }
