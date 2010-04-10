5111753995b10632be338af89887bfa70201d7d1
Sean Proctor <sproctor@gmail.com>
2010-04-09 23:21:15 -0400

    Implement "presets" (highlights from the server) in styled tags.

--- stormfront/cc.warlock.core.stormfront/src/main/cc/warlock/core/stormfront/internal/StyledSubTagHandler.java
+++ stormfront/cc.warlock.core.stormfront/src/main/cc/warlock/core/stormfront/internal/StyledSubTagHandler.java
@@ -1,26 +1,22 @@
 package cc.warlock.core.stormfront.internal;
 
-import java.util.HashMap;
 import java.util.Stack;
 
 import cc.warlock.core.client.IWarlockStyle;
 import cc.warlock.core.client.WarlockString;
 import cc.warlock.core.client.WarlockStringMarker;
+import cc.warlock.core.client.internal.WarlockStyle;
 import cc.warlock.core.stormfront.IStormFrontProtocolHandler;
 import cc.warlock.core.stormfront.IStormFrontTagHandler;
 import cc.warlock.core.stormfront.xml.StormFrontAttributeList;
 
 public abstract class StyledSubTagHandler extends DefaultTagHandler {
 
-	private HashMap<String, IStormFrontTagHandler> tagHandlers =
-		new HashMap<String, IStormFrontTagHandler>();
 	protected WarlockString buffer;
 	private Stack<WarlockStringMarker> styleStack = new Stack<WarlockStringMarker>();
 	
 	public StyledSubTagHandler(IStormFrontProtocolHandler handler) {
 		super(handler);
-		
-		tagHandlers.put("b", new StyleBTagHandler());
 	}
 	
 	@Override
@@ -45,7 +41,12 @@ public abstract class StyledSubTagHandler extends DefaultTagHandler {
 	
 	@Override
 	public IStormFrontTagHandler getTagHandler(String tagName) {
-		return tagHandlers.get(tagName);
+		if(tagName.equals("b"))
+			return new StyleBTagHandler();
+		if(tagName.equals("preset"))
+			return new StylePresetTagHandler();
+		
+		return null;
 	}
 	
 	private void addStyle(WarlockStringMarker marker) {
@@ -89,13 +90,45 @@ public abstract class StyledSubTagHandler extends DefaultTagHandler {
 		@Override
 		public void handleEnd(String rawXML) {
 			removeStyle(marker);
-			marker = null;
 		}
 		
 		@Override
 		public boolean ignoreNewlines() {
 			return false;
 		}
+	}
+	
+	public class StylePresetTagHandler extends BaseTagHandler {
+		
+		private WarlockStringMarker marker = null;
+		
+		@Override
+		public String[] getTagNames() {
+			return new String[] { "preset" };
+		}
+		
+		@Override
+		public void handleStart(StormFrontAttributeList attributes, String rawXML) {
+			String id = attributes.getValue("id");
+			IWarlockStyle style = handler.getClient().getClientSettings().getNamedStyle(id);
+			if (style == null)
+				style = new WarlockStyle();
+
+			style.setName(id);
+			marker = new WarlockStringMarker(style, buffer.length(), buffer.length());
+			addStyle(marker);
+		}
+		
+		@Override
+		public void handleEnd(String rawXML) {
+			removeStyle(marker);
+		}
+		
+		@Override
+		public  boolean ignoreNewlines() {
+			return false;
+		}
 
 	}
+
 }
